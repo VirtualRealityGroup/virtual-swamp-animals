60856da1c5b820fca32e5835bdb2c7487e432dbe
EvanMAmstutz <evangolf1@ufl.edu>
2016-01-29 09:30:49 -0500

    Frog Movement Script
    I attempted to make the frog move where the camera was looking

---
+++ Project 1/Assets/Move_Frog_Where_Looking.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Frog_Where_Looking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
