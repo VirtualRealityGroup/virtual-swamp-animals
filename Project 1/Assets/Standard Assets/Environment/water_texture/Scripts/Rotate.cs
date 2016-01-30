d9c796a4b62a818272789c3e7325b94d71875787
Reid <reidgill20@gmail.com>
2016-01-30 17:27:16 -0500

    fixed un-wanted deletions

---
+++ Project 1/Assets/Standard Assets/Environment/water_texture/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public float rotationsPerMinute = 10.0f; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			transform.Rotate(0.0f, 6.0f * rotationsPerMinute * Time.deltaTime, 0.0f);
+	}
+}
