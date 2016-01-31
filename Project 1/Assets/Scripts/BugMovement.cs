67fc16c487585eb776992a2aec8f60185a7b2567
Reid <reidgill20@gmail.com>
2016-01-31 14:12:23 -0500

    First person movement and rotation added, made with two scripts. Still an issue with the attatchment of camera to the POV character

---
+++ Project 1/Assets/Scripts/BugMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugMovement : MonoBehaviour {
+
+	public float speed = 5;
+	
+	public Cardboard cardB;
+	public CardboardHead head;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Quaternion povRot = cardB.HeadPose.Orientation; 
+		transform.rotation = Quaternion.Euler(0, povRot.eulerAngles.y, 0);
+
+
+
+		var v3 = speed * head.Gaze.direction;
+		v3.y = 0.0f;
+		transform.position += v3;
+
+	}
+}
--- Project 1/Assets/Scripts/CameraController.cs
+++ Project 1/Assets/Scripts/CameraController.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 	
 	public GameObject player;
-	
+	public float offsetX;
+	public float offsetY;
+	public float offsetZ;
+
 	private Vector3 offset;
+
 	
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+		offset = new Vector3(offsetX, offsetY, offsetZ);
+		//offset = transform.position - player.transform.position;
 	}
 	
 	// LateUpdate is called once per frame, guaranteed to run after all items have been processed in update
