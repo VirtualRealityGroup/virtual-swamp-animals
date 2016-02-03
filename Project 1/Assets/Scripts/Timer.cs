4ba775c5494a8a00dc7c36d1f8923f2014afa80a
Reid <reidgill20@gmail.com>
2016-02-03 07:14:55 -0500

    Implemented timer script for scripting things at various time intervals. Currently in middle of testing, offset for camera originally at y (.6) for bug.

---
+++ Project 1/Assets/Scripts/Timer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float timeElapsed = 0.0f;
+	private bool tenSec = false;
+
+
+     public GameObject[] ZeroMinsVisibleList;
+     public GameObject[] TwoMinsVisibleList;
+     
+     void Update()
+     {
+         timeElapsed += Time.deltaTime;
+         if(timeElapsed > 10.0f && tenSec == false)
+         {
+         	tenSec = true;
+
+             twoMinutes();
+         }
+     }
+ 
+ 	//For frog
+     void twoMinutes() {
+
+         //here's the script you need to create an array of gameobjects with tag: Appear
+         if (ZeroMinsVisibleList.Length == 0) {
+
+             ZeroMinsVisibleList = GameObject.FindGameObjectsWithTag("ZeroMins");
+
+         }
+
+         if (TwoMinsVisibleList.Length == 0) {
+
+			TwoMinsVisibleList = GameObject.FindGameObjectsWithTag("TwoMins");
+         }
+
+         
+         //set each object's Mesh Rendere to enabled that appears in the list
+
+         foreach (GameObject visible in ZeroMinsVisibleList) {
+         	 
+             visible.SetActive(false);
+             
+         }
+
+         foreach (GameObject visible in TwoMinsVisibleList) {
+
+             visible.SetActive(true);
+             
+         }
+
+ }
+
+
+}
